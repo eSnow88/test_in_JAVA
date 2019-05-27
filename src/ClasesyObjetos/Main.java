260e0a1e907cf8da60449899b2d83ef6b4a54396
Eduardo <eduardo@MacBook-Pro-de-Eduardo.local>
2019-05-27 05:50:40 -0600

    first_commit

---
+++ src/ClasesyObjetos/Main.java
@@ -0,0 +1,17 @@
+package ClasesyObjetos;
+
+public class Main {
+    public static void main(String[] args) {
+        int x;
+        int y;
+        int mayor;
+
+        x = 110;
+        y = 20;
+
+        mayor = (x > y) ? x : y;
+
+        System.out.println("El mayor es: " + mayor);
+
+    }
+}
---
+++ src/ClasesyObjetos/Operacion.java
@@ -0,0 +1,13 @@
+package ClasesyObjetos;
+public class Operacion {
+    int numero1;
+    int numero2;
+    int suma;
+    int resta;
+    int multiplicacion;
+    int division;
+
+    public void leerNumeros(){
+        
+    }
+}
